fdb6deec1b86e272414c4572785e289e58598d35
Sebastian <91731156+0Sebbe@users.noreply.github.com>
2023-04-25 14:37:24 +0200

    Add files via upload

--- slutprojekt sociala medier/regLogin.php
+++ slutprojekt sociala medier/regLogin.php
@@ -3,14 +3,18 @@ $loginNamn = $_POST["loginNamn"];
 $loginLösenord = $_POST["loginLösenord"];
 
 $db = new SQLite3('Användare.sq3');
-
-if($db->exec("SELECT * FROM ANVÄNDARE WHERE användarnamn LIKE $loginNamn") && $db->exec("SELECT * FROM ANVÄNDARE WHERE användarnamn LIKE $loginNamn"))
+$Användare = $db ->query("SELECT * FROM ANVÄNDARE;");
+while ($row = $Användare->fetchArray(SQLITE3_ASSOC))
 {
+	if($row['användarnamn'] == $loginNamn && $row['lösenord'] == $loginLösenord )
+	{
 	header("Location: MainHTML.php");
-}
-else
-{
+	}
+	else
+	{
 	header("Location: LogInHTML.php");
+	}
 }
+
 
 ?>
